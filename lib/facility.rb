8c1a98d1fb49b7ec162bd0e077cdb2901a883293
Logan Sauer <ldsauer2@gmail.com>
2024-12-16 11:50:12 -0700

    about halfway through second iteration for facility

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,42 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services, 
+              :registered_vehicles,
+              :collected_fees
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(facility)
+    @name = facility[:name]
+    @address = facility[:address]
+    @phone = facility[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
   end
+
+  def register_vehicle(vehicle)
+
+    if @services.include?('Vehicle Registration')
+      @registered_vehicles << vehicle
+    end
+
+    vehicle.registration_date = Date.today
+
+    if vehicle.antique?
+      vehicle.plate_type = :antique
+      @collected_fees += 25
+    elsif vehicle.electric_vehicle?
+      vehicle.plate_type = :ev
+      @collected_fees += 200
+    else vehicle.plate_type = :regular
+      @collected_fees += 100
+    end
+
+  end
+
 end
