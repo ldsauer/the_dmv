3c471da6eac709d3d7cf779a79be6b52afa256c8
Logan Sauer <ldsauer2@gmail.com>
2024-12-17 16:12:33 -0700

    had to fix some merge conflicts

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -4,14 +4,22 @@ class VehicleFactory
     def create_vehicles(vehicle_data)
         vehicle_data.map do |data|
           Vehicle.new = {
-            :vin
-            :year
-            :make
-            :model 
+            :vin => :vin, 
+            :year => :year, 
+            :make => :make,
+            :model => :model, 
             :engine => :ev
           }
         end
     end
+
+
+
+
+
+
+
+
 end
 
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,8 @@ require './lib/dmv'
 require './lib/facility'
 require './lib/vehicle'
 require './lib/dmv_data_service'
+
+
+
 require './lib/registrant'
 require './lib/vehicle_factory'
